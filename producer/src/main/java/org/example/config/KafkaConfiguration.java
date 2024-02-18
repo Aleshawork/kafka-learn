8d3d30dd0d233d13fc8eaa3e210356d2e4fa1a2b
Borisov Alexey <AMiBorisov@sberbank.ru>
2024-02-18 16:33:49 +0300

    add custome partitioner

--- producer/src/main/java/org/example/config/KafkaConfiguration.java
+++ producer/src/main/java/org/example/config/KafkaConfiguration.java
@@ -3,6 +3,7 @@ package org.example.config;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.example.CustomByNumberPartitioner;
 import org.example.KafkaProperties;
 import org.example.UserAction;
 import org.example.UserActionSerializer;
@@ -20,6 +21,7 @@ public class KafkaConfiguration {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
         props.put(ProducerConfig.CLIENT_ID_CONFIG, properties.getProducerName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomByNumberPartitioner.class);
         return new KafkaProducer<>(props, null, userActionSerializer);
     }
 
